63e70ea29d8d43c4bc0a1923fed8d9713e21c524
Dezsö Babai <dezso.babai@tum.de>
2024-08-17 16:09:52 +0200

    fixed kernel
    Signed-off-by: Dezsö Babai <dezso.babai@tum.de>

--- platforms/common/src/kernels/cutoffperiodicTorsionForce.cc
+++ platforms/common/src/kernels/cutoffperiodicTorsionForce.cc
@@ -1,7 +1,3 @@
-real3 vdist = make_real3(pos5.x-pos6.x, pos5.y-pos6.y, pos5.z-pos6.z);
-real dist2 = vdist.x*vdist.x + vdist.y*vdist.y + vdist.z*vdist.z;
-real dist = SQRT(dist2);
-
 float4 torsionParams = PARAMS[index];
 real deltaAngle = torsionParams.z*theta-torsionParams.y;
 energy += torsionParams.x*(1.0f+COS(deltaAngle))*torsionParams.w;
